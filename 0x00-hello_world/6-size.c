d38b8f6d638f676636d2faf98d18101e63fc87e1
violajowa <viola.jowa@gmail.com>
2023-02-17 07:47:26 -0800

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-    printf("Size of char: %lu byte(s)\n", sizeof(char));
-    return (0);
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+return (0);
 }
