1ec4fa910ff3088efa4146158dbcce8e4babaa26
violajowa <viola.jowa@gmail.com>
2023-02-18 00:20:03 -0800

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-printf("Size of a char: %li byte(s)\n", sizeof(char));
-printf("Size of an int: %li byte(s)\n", sizeof(int));
-printf("Size of a long int: %li byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 return (0);
 }
