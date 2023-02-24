e5b49a5ca0448712ab74ac532bb428b976328d12
violajowa <viola.jowa@gmail.com>
2023-02-23 23:10:01 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,25 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 != 0)
+if((i % 3 ==0) && (i % 5 ==0))
 {
-printf(" Fizz");
+printf("Fizzbuzz");
 }
-else if (i % 5 == 0 && i % 3 != 0)
+else if(i % 3 ==0)
 {
-printf(" Buzz");
+printf("Fizz");
 }
-else if (i % 3 == 0 && i % 5 == 0)
+else if(i % 5 == 0)
 {
-printf(" FizzBuzz");
-} 
-else if (i == 1)
-{
-printf("%d", i);
+printf("Buzz");
 }
 else
 {
-printf(" %d", i);
+printf("%d",i);
+}
+if(i!=100)
+{
+printf(" ");
 }
 }
 printf("\n");
